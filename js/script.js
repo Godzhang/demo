b7839a84d25ecdb4a6c471582a9f3d90bff505af
godzhang <zhangqijilindaxue@126.com>
2017-09-26 01:36:48 +0800

    new effect

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 window.onload = function(){
 	var container = document.querySelector(".container"),
 		box = container.querySelector(".box"),	    //列表外层容器
@@ -10,49 +9,6 @@ window.onload = function(){
 		lineHeight = li[0].offsetHeight || 32;      
 		flag = false,
 		prevNode = null; //保存前一个元素
-=======
-//class选择器，只返回第一个匹配项
-function $(className){
-	className = className.slice(1);
-	
-	var elems = document.getElementsByTagName("*");
-	var reg = new RegExp("(^|\\s)" + className + "(\\s|$)");
-
-	for(var i = 0, len = elems.length; i < len; i++){
-		var elem = elems[i];
-		if(reg.test(elem.className)){
-			return elem;
-		}
-	}
-};
-
-function addEvent(elem, type, handler){
-	if(document.addEventListener){
-		elem.addEventListener(type, handler, false);
-	}else{
-		elem.attachEvent('on' + type, handler);
-	}
-};
-
-window.onload = function(){
-	var container = $(".container"),
-		box = $(".box"),	    //列表外层容器
-		list = $(".list"),    //列表
-		li = list.getElementsByTagName("li"),	    //列表项
-		frame = $(".frame"),  //选中框
-		result = $(".result"),
-		frameTop = frame.offsetTop,		            //中间框的top值
-		lineHeight = li[0].offsetHeight || 32;      
-		// lineHeight = 20;
-		flag = false,
-		prevNode = null, //保存前一个元素
-		timer = null;
-
-	//禁止元素被选中,IE9-
-	if(document.all){
-		document.body.onselectstart = document.body.ondrag = function(){return false;}
-	}
->>>>>>> 5cbbdfd57502babe85ab95d7d19704070fca34f6
 
 	//初始化列表位置
 	box.style.top = frameTop + "px";
@@ -68,10 +24,6 @@ window.onload = function(){
 
 		switch(event.type){
 			case "mousedown":
-<<<<<<< HEAD
-=======
-				clearInterval(timer);
->>>>>>> 5cbbdfd57502babe85ab95d7d19704070fca34f6
 				flag = true;
 				startY = event.clientY;  //点击位置坐标
 				listTop = list.offsetTop;//记录初始top
@@ -80,11 +32,7 @@ window.onload = function(){
 				if(flag){
 					diffY = event.clientY - startY;
 					list.style.top = listTop + diffY + "px";
-<<<<<<< HEAD
 				}				
-=======
-				}
->>>>>>> 5cbbdfd57502babe85ab95d7d19704070fca34f6
 				break;
 			case "mouseup":
 				flag = false;
@@ -99,10 +47,6 @@ window.onload = function(){
 			listHeight = list.offsetHeight,		   //列表height
 			diffVal,							   //偏差距离
 			halfVal = Math.round(lineHeight / 2);  //判断依据
-<<<<<<< HEAD
-=======
-		var distance;
->>>>>>> 5cbbdfd57502babe85ab95d7d19704070fca34f6
 
 		//保证列表内容在选中框内
 		if(listEndTop > 0){
@@ -121,47 +65,20 @@ window.onload = function(){
 			list.style.top = listEndTop + diffVal + "px";
 		}
 	}
-<<<<<<< HEAD
 	//利用调整位置后的数据计算选中项
 	function check(){
 		var listEndTop = list.offsetTop,
 			diffVal = Math.abs(listEndTop) / lineHeight;
-=======
-
-	function animateTo(top, distance){
-		
-	};
-
-	//利用调整位置后的数据计算选中项
-	function check(){
-		var listEndTop = list.offsetTop,
-			diffVal;
-
-		if(listEndTop > 0){
-			diffVal = 0;
-		}else{
-			diffVal = Math.abs(listEndTop) / lineHeight;
-		}
->>>>>>> 5cbbdfd57502babe85ab95d7d19704070fca34f6
 
 		//初始判断
 		if(!prevNode){
 			prevNode = li[diffVal];
-<<<<<<< HEAD
 			prevNode.classList.add("act");
 		}
 
 		if(prevNode != li[diffVal]){
 			prevNode.classList.remove("act");
 			li[diffVal].classList.add("act");
-=======
-			prevNode.className = "act";
-		}
-
-		if(prevNode != li[diffVal]){
-			prevNode.className = "";
-			li[diffVal].className = "act";
->>>>>>> 5cbbdfd57502babe85ab95d7d19704070fca34f6
 			prevNode = li[diffVal];		//保存选中元素
 		}
 		
@@ -170,20 +87,12 @@ window.onload = function(){
 
 	function wheelDelta(event){
 		event = event || window.event;
-<<<<<<< HEAD
-=======
-		//防止页面滚动
->>>>>>> 5cbbdfd57502babe85ab95d7d19704070fca34f6
 		if(event.preventDefault){
 			event.preventDefault();
 		}else{
 			event.returnValue = false;
 		}
-<<<<<<< HEAD
 		
-=======
-
->>>>>>> 5cbbdfd57502babe85ab95d7d19704070fca34f6
 		var listTop = list.offsetTop,
 			delta = event.wheelDelta || -event.detail;
 
@@ -199,17 +108,9 @@ window.onload = function(){
 		check();
 	}
 
-<<<<<<< HEAD
 	container.addEventListener("mousewheel", wheelDelta, false);
 	container.addEventListener("DOMMouseScroll", wheelDelta, false);
 	container.addEventListener("mousedown", moveList, false);
 	document.addEventListener("mousemove", moveList, false);
 	document.addEventListener("mouseup", moveList, false);
-=======
-	addEvent(container, "mousewheel", wheelDelta);
-	addEvent(container, "DOMMouseScroll", wheelDelta);
-	addEvent(container, "mousedown", moveList);
-	addEvent(document, "mousemove", moveList);
-	addEvent(document, "mouseup", moveList);;
->>>>>>> 5cbbdfd57502babe85ab95d7d19704070fca34f6
 }
